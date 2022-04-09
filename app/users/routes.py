b661c55b7154eb437166357e5222e83727470517
Melvin2306 <melvin.rinkleff@gmx.de>
2022-04-09 14:14:54 +0200

    Update routes.py

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template, url_for, request, current_app
+from .models import User, Company
 
 blueprint = Blueprint('simple_pages', __name__)
 
@@ -6,4 +7,4 @@ blueprint = Blueprint('simple_pages', __name__)
 @blueprint.route('/<user_id>')
 def user(user_id):
     return render_template('/users/user.html', user_id=user_id)
-
+    
