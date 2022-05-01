88fc1a7d05833e827d90a4d2dfe1104d5741d568
Melvin2306 <melvin.rinkleff@gmx.de>
2022-05-02 00:09:06 +0200

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -2,3 +2,23 @@ from app.extensions.database import db, CRUDMixin
 from flask_login import UserMixin
 import uuid
 
+class User(db.Model, CRUDMixin, UserMixin):
+    id = db.Column('id', db.Text, default=lambda: str(uuid.uuid4()), primary_key=True)
+    user_email = db.Column(db.String(40), index = True, unique = True)
+    password = db.Column(db.Text)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    street_name = db.Column(db.String(40))
+    zip_code = db.Column(db.Numeric(8, 0))
+    town = db.Column(db.String(20))
+    country = db.Column(db.String(20))
+    companies = db.relationship('Company', backref='user', lazy=True)
+
+class Company(db.Model, CRUDMixin):
+    id = db.Column('id', db.Text, default=lambda: str(uuid.uuid4()), primary_key=True)
+    user_id = db.Column(db.Text, db.ForeignKey('user.id'))
+    name = db.Column(db.String(30))
+    category = db.Column(db.String(20))
+    company_email = db.Column(db.String(40))
+    phone_number = db.Column(db.String(20))
+    description = db.Column(db.String(100))
