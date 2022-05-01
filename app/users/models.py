cc0b6ebf7446bfa5139e382539b6bbc86edfa062
Melvin2306 <melvin.rinkleff@gmx.de>
2022-05-01 23:53:11 +0200

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -5,6 +5,7 @@ import uuid
 class User(db.Model, CRUDMixin, UserMixin):
     id = db.Column('id', db.String(999), default=lambda: str(uuid.uuid4()), primary_key=True)
     user_email = db.Column(db.String(40), index = True, unique = True)
+    password = db.Column(db.String(120))
     first_name = db.Column(db.String(30))
     last_name = db.Column(db.String(30))
     street_name = db.Column(db.String(40))
